edd0508a4388f0fd6e3918cbc5a211caf5b0c4ff
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-02-17 16:59:49 +0300

    switch from less to postcss

---
+++ build/postcss.js
@@ -0,0 +1,26 @@
+const fs = require('node:fs/promises')
+const path = require('node:path')
+const postcss = require('postcss')
+const presetEnv = require('postcss-preset-env')
+const atImport = require('postcss-import')
+
+const entrypoint = path.join(__dirname, '../src/main.css')
+const target     = path.join(__dirname, '../dist/missing.css')
+
+const pc = postcss([
+	atImport(),
+  presetEnv({ browsers: 'last 5 versions', stage: 1 }),
+])
+
+fs.readFile(entrypoint, { encoding: 'utf8' })
+	.then(css =>
+		pc.process(
+			css,
+			{
+				from: entrypoint,
+				to: target,
+			},
+		)
+	).then(result => {
+		fs.writeFile(target, result.css)
+	})
