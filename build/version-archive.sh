eb61e819ee1e5260916f15b98fc7dde3110427a0
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-05-16 17:16:15 +0300

    version archive: check for needed commands

--- build/version-archive.sh
+++ build/version-archive.sh
@@ -3,24 +3,23 @@ set -e
 
 root_dist=$(realpath dist)
 
+command -v git grep npm
+
 rm -rf dist/src.tmp.d
 git clone . dist/src.tmp.d
-cd dist/src.tmp.d
+(
+    cd dist/src.tmp.d
+    { echo master; git tag --list | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+'
+    } | while read tag
+        do
+            echo "${0}: Checking out $tag"
+            git checkout $tag
+            echo "${0}: Building $tag"
+            npm run build
 
-{
-    echo master
-    git tag --list | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+'
-} | while read tag
-    do
-        echo "${0}: Checking out $tag"
-        git checkout $tag
-        echo "${0}: Building $tag"
-        npm run build
-
-        echo "${0}: Built, placing into /$tag/"
-        mkdir -p $root_dist/archive/$tag
-        cp dist/missing.* $root_dist/archive/$tag/
-    done
-
-cd ../..
+            echo "${0}: Built, placing into /$tag/"
+            mkdir -p $root_dist/archive/$tag
+            cp dist/missing.* $root_dist/archive/$tag/
+        done
+)
 rm -rf dist/src.tmp.d
