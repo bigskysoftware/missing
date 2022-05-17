3487ef5ff35d6d13a057fda7f4cb3e166e35002c
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-05-17 20:29:48 +0300

    version archive stuff

--- build/version-archive.sh
+++ build/version-archive.sh
@@ -3,23 +3,22 @@ set -e
 
 root_dist=$(realpath dist)
 
-command -v git grep npm
+command -v git grep deno
 
 rm -rf dist/src.tmp.d
 git clone . dist/src.tmp.d
 (
     cd dist/src.tmp.d
-    { echo master; git tag --list | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+'
-    } | while read tag
-        do
-            echo "${0}: Checking out $tag"
-            git checkout $tag
-            echo "${0}: Building $tag"
-            [ -f ./_build/postcss.ts ] && ./_build/postcss.ts || ./_build/postcss.js
+    git tag --list | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+' | while read tag
+    do
+        echo "${0}: Checking out $tag"
+        git checkout $tag
+        echo "${0}: Building $tag"
+        deno task build-css
 
-            echo "${0}: Built, placing into /$tag/"
-            mkdir -p $root_dist/archive/$tag
-            cp dist/missing.* $root_dist/archive/$tag/
-        done
+        echo "${0}: Built, placing into /$tag/"
+        mkdir -p $root_dist/archive/$tag
+        cp dist/missing.* $root_dist/archive/$tag/
+    done
 )
 rm -rf dist/src.tmp.d
