1a8a87b907c21d9643c0c1006020137798f1ddcc
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-06-11 02:51:26 +0300

    version archive script: use temp dir

--- build/version-archive.sh
+++ build/version-archive.sh
@@ -2,13 +2,20 @@
 set -e
 
 root_dist=$(realpath dist)
+dir=$(mktemp -d)
 
 command -v git grep deno
 
-rm -rf dist/src.tmp.d
-git clone . dist/src.tmp.d
+cleanup() {
+    rm -rf dist/src.tmp.d
+}
+
+trap cleanup EXIT
+cleanup
+
+git clone . $dir
 (
-    cd dist/src.tmp.d
+    cd $dir
     git tag --list | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+' | while read tag
     do
         echo "${0}: Checking out $tag"
@@ -21,5 +28,3 @@ git clone . dist/src.tmp.d
         cp dist/missing.* $root_dist/archive/$tag/
     done
 )
-rm -rf dist/src.tmp.d
-deno task css
