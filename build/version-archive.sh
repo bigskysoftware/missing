cb1dc79f2ee79b7c536b76155e7d07101c340e58
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-06-30 01:48:20 +0300

    hhhhhh

--- build/version-archive.sh
+++ build/version-archive.sh
@@ -14,17 +14,18 @@ trap cleanup EXIT
 cleanup
 
 git clone . $dir
-(
-    cd $dir
-    git tag --list | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+' | while read tag
-    do
-        echo "${0}: Checking out $tag"
-        git switch --detach $tag
-        echo "${0}: Building $tag"
-        deno task css
 
-        echo "${0}: Built, placing into /$tag/"
-        mkdir -p $root_dist/archive/$tag
-        cp dist/missing* $root_dist/archive/$tag/
-    done
-)
+cd $dir
+echo "Entered $PWD"
+git tag --list | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+' | while read tag
+do
+    echo "${0}: Checking out $tag"
+    git switch --detach $tag
+    echo "${0}: Building $tag"
+    deno task css
+
+    echo "${0}: Built, placing into /$tag/"
+    mkdir -p $root_dist/archive/$tag
+    cp dist/missing* $root_dist/archive/$tag/
+done
+
