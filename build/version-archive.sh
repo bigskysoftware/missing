ab618522d12c935d3341b76398d0358bf40a5010
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-07-11 18:22:31 +0300

    clean up version archive script

--- build/version-archive.sh
+++ build/version-archive.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 set -e
+{
+    command -v git
+    command -v grep
+    command -v deno
+} >&-
 
-root_dist=$(realpath dist)
-dir=$(mktemp -d)
+project_root=$(realpath .)
+repo=$project_root # TODO: add fast path of cloning from existing repo (doesn't work in netlify build)
+cd $(mktemp -d)
+git clone $repo .;                                 echo "Cloned into $PWD" >&2
 
-command -v git
-command -v grep
-command -v deno
+git tag --list \
+| grep -E '^v[0-9]+\.[0-9]+\.[0-9]+' \
+| while read tag; do
+    echo "# Tag $tag #" >&2
+    git switch --detach $tag;                      echo "Checked out $tag" >&2
 
-git clone https://github.com/bigskysoftware/missing $dir
+    deno task css;                                 echo "Built $tag" >&2
 
-cd $dir
-echo "Entered $PWD" >&2
-
-git tag --list | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+' | while read tag
-do
-    echo "${0}: Checking out $tag" >&2
-    git switch --detach $tag
-    echo "${0}: Building $tag" >&2
-    deno task css
-
-    echo "${0}: Built, placing into /$tag/" >&2
-    mkdir -p $root_dist/archive/$tag
-    cp dist/missing* $root_dist/archive/$tag/
+    destdir="$project_root/dist/archive/$tag/"
+    mkdir -p $destdir
+    cp dist/missing* $destdir;                     echo "Placing into /$tag/" >&2
 done
-
