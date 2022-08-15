2debecd2352db0239920b89081846fdaff38bd14
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-08-15 12:10:00 +0200

    avoid highlighting already highlighted html

--- www/_build/highlighting.ts
+++ www/_build/highlighting.ts
@@ -1,16 +1,20 @@
-
-import prismHighlight from "lume/plugins/prism.ts"
-import Prism from "lume/deps/prism.ts"
-import prismHyperscript from "https://esm.sh/prism-hyperscript"
-import type { Site } from "lume/core.ts"
+import prismHighlight from "lume/plugins/prism.ts";
+import Prism from "lume/deps/prism.ts";
+import prismHyperscript from "https://esm.sh/prism-hyperscript@1.1.1";
+import type { Site } from "lume/core.ts";
 
 /**
  * Highlight code, including _hyperscript, with Prism.
  */
 export default () => {
-    prismHyperscript(Prism)
+  prismHyperscript(Prism);
 
-    return (site: Site) => {
-        site.use(prismHighlight({ cssSelector: "code" }))
-    }
-}
+  return (site: Site) => {
+    site.use(
+      prismHighlight({
+        cssSelector:
+          "code:not(.highlighted)", // avoid already highlighted code
+      }),
+    );
+  };
+};
