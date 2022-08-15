fc4812f8af7b8a1a9dd0ee94f8cb368fe2700ed4
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-08-15 10:55:42 +0200

    Rewrite version archive script in TypeScript

---
+++ build/version-archive.ts
@@ -0,0 +1,61 @@
+#!/usr/bin/env -S deno run -A
+
+import $ from "https://deno.land/x/dax@0.9.0/mod.ts";
+
+const projectRoot = $.path.join($.path.fromFileUrl(import.meta.url), "../..");
+const repo = "https://github.com/bigskysoftware/missing"; // TODO: add fast path of cloning from existing repo (doesn't work in netlify build)
+let clonedRepo: string;
+
+async function buildVersion(gitTag: string) {
+  const dest = $.path.join(projectRoot, "/dist/archive/", gitTag);
+  await Deno.mkdir(dest, { recursive: true });
+  $.logLight("Destination:", dest);
+  const cachePath = $.path.join(
+    projectRoot,
+    ".missingcss-releases-cache",
+    gitTag,
+  );
+
+  if (await $.exists(cachePath)) {
+    $.logLight("Found", gitTag, "in release cache: ", cachePath);
+    $.logStep("Copying", gitTag, "artifacts from cache");
+    await $.fs.copy(cachePath, dest, { overwrite: true });
+  } else {
+    if (!clonedRepo) {
+      clonedRepo = await Deno.makeTempDir();
+      $.logStep(`Cloning repo into ${clonedRepo}`);
+      await $.cd(clonedRepo);
+      await $`git clone ${repo} .`.quiet();
+    }
+
+    $.logStep("Checking out", `${gitTag}`);
+    await $`git switch --detach ${gitTag}`.quiet();
+
+    $.logStep(`Building ${gitTag}`);
+    await $`deno task css`.quiet();
+
+    $.logStep(`Caching ${gitTag} artifacts`);
+    $.logStep(`Placing ${gitTag} artifacts into archive/${gitTag}`);
+    await Deno.mkdir(cachePath, { recursive: true });
+    for await (const file of $.fs.expandGlob("missing*", { root: "dist " })) {
+      await Promise.all([
+        $`cp ${file} ${cachePath}`.quiet(),
+        $`cp ${file} ${cachePath}`.quiet(),
+      ]);
+    }
+  }
+}
+
+const gitProcess = Deno.run({
+  cmd: ["git", "tag", "--list"],
+  stdout: "piped",
+});
+
+for (const line of await $`git tag --list`.lines()) {
+  if (/^v\d+\.\d+\.\d+/.test(line)) {
+    $.logStep(`Building ${line}`);
+    await $.logIndent(() => buildVersion(line));
+  }
+}
+
+gitProcess.close();
