eca5d71ce632ea1b718e2283b45542b781f60a0d
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-10-22 20:57:36 +0300

    dev stuff

---
+++ analysis.sh
@@ -0,0 +1,12 @@
+
+CLASS_RE='\.([a-zA-Z0-9-_]|\\.)*(?=\s*[{,])'
+
+function selectors {
+    grep -Poh "$CLASS_RE" "$@" | sort | uniq
+}
+
+function is-in-docs {
+    grep -F "$1" docs/* >/dev/null
+}
+
+selectors src/?0*.css | sed 's/\\//' | while read sel; do is-in-docs "$sel" || echo "$sel"; done
