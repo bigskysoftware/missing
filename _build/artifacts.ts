571b3b9fd1c0eda8d44b4f0b73cc48f9c5802c6f
Deniz Akşimşek <deniz@denizaksimsek.com>
2023-01-10 18:26:33 +0300

    1.0.6 artifacts

--- _build/artifacts.ts
+++ _build/artifacts.ts
@@ -1,20 +1,22 @@
 #!/usr/bin/env -S deno run --allow-read=. --allow-write=.
 
-import { join, basename } from "std/path/mod.ts";
+import { join, basename, dirname } from "std/path/mod.ts";
 import { extract } from "lume/deps/front_matter.ts";
 
 const distDir = "dist";
-const artifactsDir = "releases/artifacts";
+const artifactsDir = "releases/_artifacts";
 
 function artifact(release: string, artifact: string) {
     const src = join(distDir, artifact);
     const dst = join(artifactsDir, release, artifact);
+    Deno.mkdirSync(dirname(dst), { recursive: true });
     return Deno.copyFile(src, dst);
 }
 
 function release(filePath: string) {
     const release = "v" + basename(filePath).split(".").slice(0, -1).join(".");
-    const frontmatter = extract(filePath).attrs;
+    const text = Deno.readTextFileSync(filePath);
+    const frontmatter = extract(text).attrs;
     const artifactsObject = frontmatter["artifacts"] as Tree<string>;
     const artifacts = leaves(artifactsObject);
     artifacts.forEach(a => artifact(release, a));
