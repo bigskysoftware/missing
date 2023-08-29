c5c7cf7cd2a82c623588068900af6e94054bafec
Deniz Akşimşek <deniz@denizaksimsek.com>
2023-08-29 15:26:28 +0300

    switch to lightningcss

--- www/missingcss.tmpl.ts
+++
@@ -1,6 +0,0 @@
-import { compile } from "../_build/postcss.ts";
-export default async function* () {
-  for (const [filename, content] of Object.entries(await compile())) {
-    yield { url: "/" + filename, content };
-  }
-}
