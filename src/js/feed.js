accd35e58db7444a4413c68620e39bd793e36325
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2025-03-18 20:50:16 -0500

    Add aria-feed custom element

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,85 +1,114 @@
 /// <reference lib="es2022" />
 
-import { $, $$, on, dispatch, halts, attr, next, prev, asHtml, hotkey, behavior, makelogger } from "./19.js"
-
-const ilog = makelogger("feed");
-const sFeed = "[role=feed]";
-const sScopedArticle = ":scope > article, :scope > [role=article]";
-const sArticle = "article, [role=article]";
-const sFocusable = ":is(a, area)[href], :is(button, details, embed, iframe, label, select, textarea):not([disabled]), :is(audio, video)[controls], :is(img, object)[usemap], [tabindex]:not([tabindex='-1'])"
-
-/**
- * @param {HTMLElement} feed
- * @returns {HTMLElement[]}
- */
-const articles = feed => $$(feed, sScopedArticle);
+import { $, $$, on, off, halts, next, prev, asHtml, hotkey } from './19.js'
 
 
-/**
- * @param {HTMLElement} feed
- * @param {HTMLElement} activeElement
- * @returns {null}
- */
-const focusPreviousArticle = (feed, activeElement) => asHtml(prev(feed, sArticle, activeElement, {wrap: false}))?.focus();
+export class ariaFeed extends HTMLElement {
+  /* Custom element implementation of the Feed Pattern as specified by the
+   * ARIA Authoring Practices Guide (APG).
+   *
+   * https://www.w3.org/WAI/ARIA/apg/patterns/feed/
+   *
+   */
 
+  static sScopedArticle = ':scope > :is(article, [role=article])'
+  static sArticle = 'article, [role=article]'
+  static sFocusable = [
+    '[tabindex]:not([tabindex="-1"])',
+    ':is(a, area)[href]',
+    ':is(audio, video)[controls]',
+    ':is(img, object)[usemap]',
+    ':is(button, details, embed, iframe, label, select, textarea):not([disabled])',
+  ].join(', ')
 
-/**
- * @param {HTMLElement} feed
- * @param {HTMLElement} activeElement
- * @returns {null}
- */
-const focusNextArticle = (feed, activeElement) => asHtml(next(feed, sArticle, activeElement.closest(sArticle), {wrap: false}))?.focus();
+  focusPreviousArticle = () => asHtml(
+    prev(this, ariaFeed.sArticle, document.activeElement, {wrap: false})
+  )?.focus()
 
+  focusNextArticle = () => asHtml(
+    next(this, ariaFeed.sArticle, document.activeElement.closest(ariaFeed.sArticle), {wrap: false})
+  )?.focus()
 
-/**
- * @param {HTMLElement} article
- * @returns {null}
- */
-const focusOutsideFeed = article => asHtml(article.parentElement.closest(sArticle))?.focus();
+  focusOutsideNestedFeed = () => asHtml(
+    document.activeElement.parentElement.closest(ariaFeed.sArticle)
+  )?.focus()
 
+  focusInsideNestedFeed = () => asHtml(
+    $(document.activeElement.closest(ariaFeed.sArticle), ariaFeed.sArticle)
+  )?.focus()
 
-/**
- * @param {HTMLElement} activeElement
- * @returns {null}
- */
-const focusInsideFeed = activeElement => asHtml($(activeElement.closest(sArticle), sArticle))?.focus();
+  focusBeforeFeed = () => asHtml(
+    prev(document.body, ariaFeed.sFocusable, this, {})
+  )?.focus()
 
+  focusAfterFeed = () => asHtml(
+    next(document.body, ariaFeed.sFocusable, this, {})
+  )?.focus()
 
-/**
- * @param {HTMLElement} feed
- * @returns {null}
- */
-const focusBeforeFeed = feed => asHtml(prev(document.body, sFocusable, feed, {}))?.focus();
+  setAriaAttributes = () => {
+    $$(this, ariaFeed.sScopedArticle).forEach((article, idx, articles) => {
+      article.setAttribute('tabindex', '0')
+      article.setAttribute('aria-posinset', idx + 1)
+      article.setAttribute('aria-setsize', articles.length)
+      if (!(article.hasAttribute('aria-labelledby') || article.hasAttribute('aria-label')))
+        console.error(
+          '<aria-feed>: Article has no accessible name (aria-label or aria-labelledby)',
+          article
+        )
+    })
+  }
 
+  setAriaInteractions = () => {
+    this.keydownListener = on(this, 'keydown', hotkey({
+      'PageUp': halts('default propagation', _ => this.focusPreviousArticle()),
+      'PageDown': halts('default propagation', _ => this.focusNextArticle()),
+      'Ctrl+Home': halts('default propagation', _ => this.focusBeforeFeed()),
+      'Ctrl+End': halts('default propagation', _ => this.focusAfterFeed()),
+      'Alt+PageUp': halts('default', _ => this.focusOutsideNestedFeed()),
+      'Alt+PageDown': halts('default', _ => this.focusInsideNestedFeed()),
+    }))
+  }
 
-/**
- * @param {HTMLElement} feed
- * @returns {null}
- */
-const focusAfterFeed = feed => asHtml(next(document.body, sFocusable, feed, {}))?.focus();
+  removeAriaInteractions = () => {
+    if (this.keydownListener) {
+      off(this.keydownListener)
+      this.keydownListener = null
+    }
+  }
 
-
-export const feed = behavior(sFeed, (feed, { root }) => {
-
-    if (!(feed instanceof HTMLElement)) return;
-
-    feed.setAttribute("aria-busy", "false");
-    articles(feed).forEach((article, idx, articles) => {
-        article.setAttribute("tabindex", "0");
-        article.setAttribute("aria-posinset", idx + 1);
-        article.setAttribute("aria-setsize", articles.length);
-        if (!(article.hasAttribute("aria-labelledby") || article.hasAttribute("aria-label")))
-            console.error("Article", article, "has no accessible name (aria-label or aria-labelledby)");
+  constructor() {
+    super()
+    this.observer = new MutationObserver(this.handleMutations.bind(this))
+    this.observer.observe(this, {
+      childList: true,
+      attributes: false,
+      characterData: false,
+      subtree: true,
     });
+  }
 
-    on(feed, "keydown", hotkey({
-        "PageUp": halts("default propagation", _ => focusPreviousArticle(feed, root.activeElement)),
-        "PageDown": halts("default propagation", _ => focusNextArticle(feed, root.activeElement)),
-        "Alt+PageUp": halts("default", _ => focusOutsideFeed(root.activeElement)),
-        "Alt+PageDown": halts("default", _ => focusInsideFeed(root.activeElement)),
-        "Ctrl+Home": halts("default propagation", _ => focusBeforeFeed(feed)),
-        "Ctrl+End": halts("default propagation", _ => focusAfterFeed(feed)),
-    }));
-});
+  connectedCallback() {
+    this.setAttribute('role', 'feed')
+    this.setAttribute('aria-live', 'polite')
+    this.setAttribute('aria-busy', 'false')
+    this.setAriaAttributes()
+    this.setAriaInteractions()
+  }
 
-feed(document);
+  disconnectedCallback() {
+    this.observer.disconnect()
+    this.removeAriaInteractions()
+  }
+
+  handleMutations(mutationsList) {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        this.setAriaAttributes()
+        this.setAriaInteractions()
+      }
+    }
+  }
+}
+
+
+customElements.define('aria-feed', ariaFeed)
