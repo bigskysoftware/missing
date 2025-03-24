63b8cedb8d6f4e7c6c9193e0a71b1645b214929b
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2025-03-24 15:25:49 -0500

    Polish

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,6 +1,6 @@
 /// <reference lib="es2022" />
 
-import { $, $$, on, off, halts, next, prev, asHtml, hotkey } from './19.js'
+import { $, $$, on, off, halts, attr, next, prev, asHtml, hotkey } from "./19.js"
 
 
 export class ariaFeed extends HTMLElement {
@@ -11,15 +11,15 @@ export class ariaFeed extends HTMLElement {
    *
    */
 
-  static sScopedArticle = ':scope > :is(article, [role=article])'
-  static sArticle = 'article, [role=article]'
+  static sScopedArticle = ":scope > :is(article, [role=article])"
+  static sArticle = "article, [role=article]"
   static sFocusable = [
-    '[tabindex]:not([tabindex="-1"])',
-    ':is(a, area)[href]',
-    ':is(audio, video)[controls]',
-    ':is(img, object)[usemap]',
-    ':is(button, details, embed, iframe, label, select, textarea):not([disabled])',
-  ].join(', ')
+    "[tabindex]:not([tabindex='-1'])",
+    ":is(a, area)[href]",
+    ":is(audio, video)[controls]",
+    ":is(img, object)[usemap]",
+    ":is(button, details, embed, iframe, label, select, textarea):not([disabled])",
+  ].join(", ")
 
   focusPreviousArticle = () => asHtml(
     prev(this, ariaFeed.sArticle, document.activeElement, {wrap: false})
@@ -45,70 +45,72 @@ export class ariaFeed extends HTMLElement {
     next(document.body, ariaFeed.sFocusable, this, {})
   )?.focus()
 
-  setAriaAttributes = () => {
+  setChildAttributes = () => {
     $$(this, ariaFeed.sScopedArticle).forEach((article, idx, articles) => {
-      article.setAttribute('tabindex', '0')
-      article.setAttribute('aria-posinset', idx + 1)
-      article.setAttribute('aria-setsize', articles.length)
-      if (!(article.hasAttribute('aria-labelledby') || article.hasAttribute('aria-label')))
-        console.error(
-          '<aria-feed>: Article has no accessible name (aria-label or aria-labelledby)',
-          article
-        )
+      attr(article, { ariaPosInSet: idx + 1, ariaSetSize: articles.length, tabindex: 0 })
+      if (!(article.hasAttribute("aria-labelledby") || article.hasAttribute("aria-label")))
+        console.error("Article", this, "has no accessible name (aria-label or aria-labelledby)");
     })
+    this._internals.ariaBusy = "false"
   }
 
-  setAriaInteractions = () => {
-    this.keydownListener = on(this, 'keydown', hotkey({
-      'PageUp': halts('default propagation', _ => this.focusPreviousArticle()),
-      'PageDown': halts('default propagation', _ => this.focusNextArticle()),
-      'Ctrl+Home': halts('default propagation', _ => this.focusBeforeFeed()),
-      'Ctrl+End': halts('default propagation', _ => this.focusAfterFeed()),
-      'Alt+PageUp': halts('default', _ => this.focusOutsideNestedFeed()),
-      'Alt+PageDown': halts('default', _ => this.focusInsideNestedFeed()),
+  setAttributes = () => {
+    this._internals.ariaRole = "feed"
+    this._internals.ariaLive = "polite"
+    this.setChildAttributes()
+  }
+
+  setInteractions = () => {
+    this.keydownListener = on(this, "keydown", hotkey({
+      "PageUp": halts("default propagation", this.focusPreviousArticle),
+      "PageDown": halts("default propagation", this.focusNextArticle),
+      "Ctrl+Home": halts("default propagation", this.focusBeforeFeed),
+      "Ctrl+End": halts("default propagation", this.focusAfterFeed),
+      "Alt+PageUp": halts("default", this.focusOutsideNestedFeed),
+      "Alt+PageDown": halts("default", this.focusInsideNestedFeed),
     }))
   }
 
-  removeAriaInteractions = () => {
+  removeInteractions = () => {
     if (this.keydownListener) {
       off(this.keydownListener)
       this.keydownListener = null
     }
   }
 
+  handleMutations = (mutationsList, observer) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        this.setChildAttributes()
+        this.setInteractions()
+      }
+    }
+  }
+
   constructor() {
     super()
-    this.observer = new MutationObserver(this.handleMutations.bind(this))
+    this._internals = this.attachInternals()
+
+    this.observer = new MutationObserver(this.handleMutations)
     this.observer.observe(this, {
       childList: true,
       attributes: false,
       characterData: false,
       subtree: true,
-    });
+    })
   }
 
   connectedCallback() {
-    this.setAttribute('role', 'feed')
-    this.setAttribute('aria-live', 'polite')
-    this.setAttribute('aria-busy', 'false')
-    this.setAriaAttributes()
-    this.setAriaInteractions()
+    this.setAttributes()
+    this.setInteractions()
   }
 
   disconnectedCallback() {
     this.observer.disconnect()
-    this.removeAriaInteractions()
+    this.removeInteractions()
   }
 
-  handleMutations(mutationsList) {
-    for (const mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        this.setAriaAttributes()
-        this.setAriaInteractions()
-      }
-    }
-  }
 }
 
 
-customElements.define('aria-feed', ariaFeed)
+customElements.define("aria-feed", ariaFeed)
